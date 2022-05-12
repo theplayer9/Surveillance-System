e03ccd3c550b75da3278a131cb21f83ac0c945cc
Om Sharma <omsharma.theplayer11@gmail.com>
2022-05-12 17:20:47 +0530

    basic navbar is ready

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { AppBar, Container, Paper, Toolbar, Typography } from '@mui/material'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <AppBar position="fixed" color="transparent">
+        <Container>
+          <Toolbar>
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{ display: 'flex' }}
+              onClick={() => {
+                ;<Link href="/index">
+                  {' '}
+                  <a></a>
+                </Link>
+              }}
+            >
+              <Image src="/img.jpg" width={70} height={70} />
+            </Typography>
+            <Paper elevation={3} component="div" sx={{ margin:"0 15px" , padding: "5px 5px"}}>
+              <Link href="/ReportComplaint">
+                <a> ReportComplaint</a>
+              </Link>
+            </Paper>
+            <Paper elevation={3} component="div" sx={{ margin:"0 15px", padding: "5px 5px" }}>
+              <Link href="/Surveillance">
+                <a> Surveillance</a>
+              </Link>
+            </Paper>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
   )
 }
 
---
+++ pages/Surveillance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Surveillance = () => {
+  return (
+    <div>Surveillance</div>
+  )
+}
+
+export default Surveillance
