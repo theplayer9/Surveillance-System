30565babdfb567afc4293ad4fdbe3518b412ffa1
Om Sharma <omsharma.theplayer11@gmail.com>
2022-05-18 18:49:33 +0530

    starting  the surveillance logic part

--- pages/Surveillance.js
+++ pages/Surveillance.js
@@ -1,10 +1,15 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState,useEffect,useRef} from 'react'
 import Navbar from './Navbar'
 import Head from 'next/head'
 import * as faceapi  from 'face-api.js'
 
 
 const Surveillance = () => {
+
+  const [initializing, setInitializing] = useState(false);
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -15,7 +20,7 @@ const Surveillance = () => {
         <Navbar />
         <p>This is the surveillance page</p>
         <div>
-
+            <span> {initializing ? " PROJECT is initializing" : "PROJECT is ready"}</span>
         </div>
       </main>
     </div>
