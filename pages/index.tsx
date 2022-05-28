a600a7e077a375ce486c54a0b892cade80ddf821
Om Sharma <omsharma.theplayer11@gmail.com>
2022-05-29 01:12:49 +0530

    set the background image

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from './Navbar'
 import { Box } from '@mui/material'
+import indexstyles from '../styles/index.module.css'
 
 const Home: NextPage = () => {
   return (
@@ -13,19 +14,29 @@ const Home: NextPage = () => {
         {/* meta tags  */}
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <Navbar />
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <h1 className="text-6xl font-bold">Find the MISSING person</h1>
-          <p className="mt-3 text-2xl">
-            Find the MISSING person
-            <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-              pages/index.tsx
-            </code>
-          </p>
-        </Box>
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 py-200 text-center">
+        <div className={indexstyles.background_image}>
+          <Image
+            src="/BackgroundIMG.png"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
+        <div className={indexstyles.somethingElse}>
+          <Navbar />
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <h1 className="text-6xl font-bold" >Find the MISSING person</h1>
+            <p className="mt-3 text-2xl" >
+              Find the MISSING person
+              {/* <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
+                pages/index.tsx
+              </code> */}
+            </p>
+          </Box>
+        </div>
       </main>
- {/* ------------footer-------------- */}
+      {/* ------------footer-------------- */}
       {/* <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
